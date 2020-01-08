2fc2437933cdae60b5e6a9cae8f90932556814f0
Sergio Velandia <sf.velandia@gmail.com>
2020-01-08 12:59:14 -0500

    counters patch

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Methods", "PATCH,GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   });
