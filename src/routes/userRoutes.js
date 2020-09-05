1da6ce290294592269dfd8dec365094c0f08c388
cebiac <sergio_velandia@cun.edu.co>
2020-09-05 00:41:56 -0500

    fix3

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -36,7 +36,7 @@ module.exports = function (app){
         res.header ='Access-Control-Allow-Credentials', true
         res.header ='Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
         var _config = req.params.presentacion;
-        User.getAlbums(_config,(err,data)=>{
+        User.getVideosFromAlbum(_config,(err,data)=>{
             res.status(200).json(data);
             
         })
