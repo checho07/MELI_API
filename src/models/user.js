66ec44b89acfe75541bfb578b6148e7a4897d85e
sergio velandia <cebiac@cun.edu.co>
2022-03-17 11:50:28 -0500

    update url

--- src/models/user.js
+++ src/models/user.js
@@ -1,112 +1,39 @@
-let Vimeo = require('vimeo').Vimeo;
-var admin = require('firebase-admin');
+
+var fAdmin = require('firebase-admin');
+var telecampusFirebase;
+const firebaseAccount = require('../../telecampus_firebase_key.json');
 let userModel = {};
-let client = new Vimeo('29f16279edeff6b5cce56553bdde6bfde3fa5bae','c7tbCj5B4Vm9jKu0mnfFGAN65Ii8mTqJashZhwVdXotEFYzMn1bJWGxI3sVCYnuuzedGyFWulb+uaE1f/kTZvtoKbqfz6148+XjRJl/8vlmowKjoL2tnd4bAsI7h1ZOd','c40b58135194a632e482b22e2461d1ee');
-require('dotenv').config();
 
 
-
-
-userModel.getAllVideosVimeo =(callback)=>{    
-
-    client.request(
-        {
-            method:'GET',
-            path: '/me/videos',
-            query:{
-                page:1,
-                per_page:100,
-                fields:'uri,files,name,description,pictures.sizes'
-            },
-            headers:{"Access-Control-Allow-Origin":"*"}
-           
-          
-        },function(error, body, status_code, headers){
-            if(error){
-                console.log('ERROR'+error)
-            };
-            callback(null,body.data)
-            console.log(body)
-        }
-        )
- }
-
- 
-
-
- userModel.getAlbums =(callback)=>{
-
-
-    client.request(
-        {
-            method:'GET',
-            path: '/me/albums',
-            query:{
-                fields:'name,metadata.connections.videos.uri'
-            },
-            headers:{"Access-Control-Allow-Origin":"*"}
-           
-          
-        },function(error, body, status_code, headers){
-            if(error){
-                console.log('ERROR'+error)
-            };
-            callback(null,body.data)
-            console.log(body)
-        }
-        )
- }   
-
- userModel.getVideosFromAlbum =(params, callback)=>{
-
-    console.log("paramsUser:"+params)
-    client.request(
-        {
-            method:'GET',
-            path: `/me/albums/${params}/videos`,
-            query:{
-                fields:'name,uri,description,duration,files',
-                direction:'asc',
-                sort:'manual'
-            },
-            headers:{"Access-Control-Allow-Origin":"*"}
-           
-          
-        },function(error, body, status_code, headers){
-            if(error){
-                console.log('ERROR'+error)
-            };
-            callback(null,body.data)
-            // console.log(body)
-        }
-        )
- } 
-
- userModel.getVideoById =(params, callback)=>{    
-
-    client.request(
-        {
-            method:'GET',
-            path: `/me/videos/${params}`,
-            query:{
-                fields:'files.link,files.width,name,description,pictures.sizes.link,duration'
-            },
-            headers:{"Access-Control-Allow-Origin":"*"}
-           
-          
-        },function(error, body, status_code, headers){
-            if(error){
-                console.log('ERROR'+error)
-            };
-            callback(null,body)
-            console.log(body)
-        }
-        )
- }
+function initializeApp() {    
+    telecampusFirebase = fAdmin.initializeApp({credential: fAdmin.credential.cert(firebaseAccount)},'Telecampus');     
+  }
+initializeApp();
 
  userModel.getMoodleUrl = (callback)=> {
-     const url = {url:'https://campusdigital.cun.edu.co/digital'}
-     callback(null, url);
+
+    let collectionRef; 
+  
+   
+  try {
+    collectionRef = telecampusFirebase.firestore().collection('telecampusMoodle').doc('urlConfig');
+    collectionRef.get().then((res)=> {
+        var url = res.data().url;
+        console.log(url)
+        callback(null, url);
+    },err => {
+        console.log(err);
+        callback(200,err);
+
+    }).catch(er=> {
+        console.log(er)
+        callback(500,er)
+    })
+  } catch (error) {
+    callback(200,error);
+  }
+   
+   
  }
 
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,59 +1,6 @@
 const User = require('../models/user')
 
-module.exports = function (app){
-
-
-    app.get('/allvideos',(req,res)=>{
-        
-        res.header = 'Access-Control-Allow-Origin', '*'
-        res.header ='Access-Control-Allow-Credentials', true
-        res.header ='Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
-    
-        
-        User.getAllVideosVimeo((err,data)=>{
-           res.status(200).json(data);
-           
-       })
-    });
-
-    app.get('/albums',(req,res)=>{        
-       
-        
-        res.header = 'Access-Control-Allow-Origin', '*'
-        res.header ='Access-Control-Allow-Credentials', true
-        res.header ='Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
-    
-        User.getAlbums((err,data)=>{
-            res.status(200).json(data);
-            
-        })
-     });
-
-     app.get('/videos/:presentacion',(req,res)=>{        
-       
-        
-        res.header = 'Access-Control-Allow-Origin', '*'
-        res.header ='Access-Control-Allow-Credentials', true
-        res.header ='Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
-        var _config = req.params.presentacion;
-        User.getVideosFromAlbum(_config,(err,data)=>{
-            res.status(200).json(data);
-            
-        })
-     });
-
-     app.get('/video/:id',(req,res)=>{        
-       
-        
-        res.header = 'Access-Control-Allow-Origin', '*'
-        res.header ='Access-Control-Allow-Credentials', true
-        res.header ='Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
-        var _config = req.params.id;
-        User.getVideoById(_config,(err,data)=>{
-            res.status(200).json(data);
-            
-        })
-     });
+module.exports = function (app){  
 
 
      app.get('/moodle',(req, res) => {
