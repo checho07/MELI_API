51aef9beb38c0f049f87a145d9f35a68f7478f87
Sergio Velandia <sergio.velandia@192.168.1.121>
2022-04-28 14:23:07 -0500

    fc

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 
+const {PORT=3000, LOCAL_ADDRESS='0.0.0.0'} = process.env
+
 //settings
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,7 +15,7 @@ app.use(function(req, res, next) {
     next();
   });
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan('dev'));
@@ -30,15 +32,20 @@ app.use(express.static('public'));
 // routes
 require('./routes/userRoutes')(app);
 
-app.listen(app.get('port'),()=>{
-    console.log('server on port'+ app.PORT);
-    /*if (err) { 
-        throw err; 
-    } else { 
-        console.log ('server on port'+ app.PORT) 
-         process.env.NODE_ENV 
-    } */
+// app.listen(app.get('port'),()=>{
+//     console.log('server on port'+ app.PORT);
+//     /*if (err) { 
+//         throw err; 
+//     } else { 
+//         console.log ('server on port'+ app.PORT) 
+//          process.env.NODE_ENV 
+//     } */
 
+// })
+
+app.listen(PORT, LOCAL_ADDRESS, () =>{
+    // const address = app.address();
+    console.log('server listening at:')
 })
 
 
