14bf50065abb72c7bf700b5a90a0e7ff878ede35
Sergio Velandia <sergio.velandia@192.168.1.121>
2022-04-28 13:43:40 -0500

    headers

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ var upload = multer();
 //settings
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "PATCH,GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   });
