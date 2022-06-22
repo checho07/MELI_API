ec1a641c99e9919fd8b321511cb415ad51e80f96
Sergio Velandia <sergio.velandia@192.168.1.121>
2022-06-21 23:26:55 -0500

    First commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,42 +1,181 @@
 
-var fAdmin = require('firebase-admin');
-var telecampusFirebase;
-const firebaseAccount = require('../../telecampus_firebase_key.json');
+
+  const { get } = require('superagent');
+const superagent = require('superagent');
+require('dotenv').config();
 let userModel = {};
 
 
-function initializeApp() {    
-    telecampusFirebase = fAdmin.initializeApp({credential: fAdmin.credential.cert(firebaseAccount)},'Telecampus');     
-  }
-initializeApp();
+const baseURL = 'https://api.mercadolibre.com/';
 
- userModel.getMoodleUrl = (callback)=> {
 
-    let collectionRef; 
+/**
+ * Funcion que ejecuta un query al API de MELI search para busqueda de items
+ * @param {*} _query string parametro de busqueda
+ * @param {*} callback retrono de la data 
+ */
+ userModel.searchItems = async (_query,callback)=> {
+
+  // Varaibles
+  let res = await queryItems(_query);
+
+  let arrayItemsFiltered = [];
+
+  let categories = [] ;
+
+  /////
+
+  /**
+   * Ciclo forEach para guardar los string de las categorias de la busqueda, 
+   * que serviran para el Bredcrumb
+   */
+  res.body.filters[0].values[0].path_from_root.forEach(element => {
+     categories.push(element.name);
+   });
+
+
+  /**
+   * Ciclo forEach para filtrar los datos necesarios de la API de MELI al frontEnd
+   */
+  res.body.results.forEach(element => {
+
+    let objFiltered = {      
+      id: element.id,
+      title: element.title,
+      price: {
+        currency:element.currency_id,
+        amount: element.price
+      },
+      picture: element.thumbnail,
+      condition: element.condition,
+      free_shipping: element.shipping.free_shipping  
+
+    }
+
+    /**Verificar que solo se obtengan los 4 primeros items */
+    if(arrayItemsFiltered.length == 4){
+
+      return;
+
+    }else{
+      
+      arrayItemsFiltered.push(objFiltered);
+    }
+
+  });
+
+
+  /**
+   * Objeto final que se enviará al frontEnd 
+   */
+  let objToSend = {
+    author:{
+      name: process.env.AUTHOR_NAME,
+      lastname: process.env.AUTHOR_LASTNAME
+    },
+    categories:categories,
+    items: arrayItemsFiltered
+
   
-   
-  try {
-    collectionRef = telecampusFirebase.firestore().collection('telecampusMoodle').doc('urlConfig');
-    
-    collectionRef.get().then((res)=> {
-        var url = res.data().url;
-        console.log(url)
-        callback(null, url);
-    },err => {
-        console.log(err);
-        callback(200,err);
-
-    }).catch(er=> {
-        console.log(er)
-        callback(500,er)
-    })
-  } catch (error) {
-    callback(200,error);
   }
-   
+
+  callback(200,objToSend);   
    
  }
 
 
+ /**
+  * Funcion que ejecuta un query al API de MELI para consultar un Item por su ID
+  * y retornar al FrontEnd un objeto con la data necesaria
+  * @param {*} _id string con el ID del item a buscar
+  * @param {*} callback 
+  */
+ userModel.getItemById = async (_id,callback)=>{
+
+    // Varaibles
+
+  let dataItem ;
+  let itemDescription;
+
+  ////
+
+  try {
+
+    /**
+     * Array de peticiones al API de MELI consultando la data del ITEM y su descripción
+     */
+    [dataItem, itemDescription] = await Promise.all([
+      queryItemById(_id),
+      getItemDescription(_id)
+      
+    ])
+
+  } catch (error) {
+    callback(error.status,'Item no encontrado')
+  } 
+
+  /**
+   * Objeto que se envia al front con la data necesaria 
+   */
+  let objToSend ={
+    author:{
+      name: process.env.AUTHOR_NAME,
+      lastname: process.env.AUTHOR_LASTNAME
+    },
+    item:{
+      id:dataItem.body.id,
+      title: dataItem.body.title,
+      price:{
+        currency:dataItem.body.currency_id,
+        amount:dataItem.body.price,        
+      },
+      picture:dataItem.body.pictures[0].url,
+      condition:dataItem.body.condition,
+      free_shipping:dataItem.body.shipping.free_shipping,
+      sold_quantity:dataItem.body.sold_quantity,
+      description: itemDescription.body.plain_text,
+    }
+  }
+  callback(200,objToSend); 
+
+ }
+
+  
+
+ 
+/**
+ * Funcion que realiza el request a la API de MELI para buscar Items
+ * @param {*} _query string con el parametro de busqueda
+ * @returns 
+ */
+ function queryItems(_query){
+
+ return  superagent.get(`${baseURL}sites/MLA/search`).query({q:_query}) 
+
+ }
+
+ /**
+  * Funcion que realiza el request a la API de MELI para buscar Item por ID
+  * @param {*} _id string ID del item
+  * @returns 
+  */
+ function queryItemById(_id){  
+
+  return superagent.get(`${baseURL}items/${_id}`)  
+ 
+}
+
+/**
+ * Funcion que realiza el request a la API de MELI para buscar la descripcion del ITEM por su ID
+ * @param {*} _id 
+ * @returns 
+ */
+function getItemDescription(_id){  
+
+  return superagent.get(`${baseURL}items/${_id}/description`)  
+ 
+}
+
+ 
 
  module.exports = userModel;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,40 @@
 const User = require('../models/user')
 
+
 module.exports = function (app){  
 
 
-     app.get('/moodle',(req, res) => {
+    /**
+     * Ruta para realizar busqueda de items 
+     */
+     app.get('/api/items',(req, res) => {
 
         res.header = 'Access-Control-Allow-Origin', '*'
         res.header ='Access-Control-Allow-Credentials', true
-        res.header ='Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
+        res.header ='Access-Control-Allow-Methods', 'GET,'
 
-        User.getMoodleUrl((err,data)=>{
+
+        let query = req.query.q; // variable que obtiene el parameto de busqueda. 
+
+        User.searchItems(query,(err,data)=>{
             res.status(200).json(data);
+        })
+     })
+
+     /**
+      * Ruta para realizar busqueda de Item por ID
+      */
+     app.get('/api/items/:id',(req, res) => {
+
+        res.header = 'Access-Control-Allow-Origin', '*'
+        res.header ='Access-Control-Allow-Credentials', true
+        res.header ='Access-Control-Allow-Methods', 'GET,'
+
+        let id = req.params.id; // variable que obtiene el ID del item a buscar
+
+        User.getItemById( id, (status,data)=>{
+            
+            res.status(status).json(data);
         })
      })
 
